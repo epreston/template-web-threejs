ea64eb1b8fccb43b09eea898934b4f254b7fda63
Ed Preston <epreston@prestonsoft.com>
2023-08-17 19:57:17 +1000

    Update vite.config.js
    cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 // vite.config.js
 
 import { defineConfig } from 'vite';
-// import { resolve } from 'path';
+// import { resolve } from 'node:path';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
   // config options
   assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.m4a'],
   server: {
-    open: '/index.html',
+    open: '/index.html'
   },
   plugins: [glsl()],
   build: {
-    target: ['es2022', 'chrome112', 'edge112', 'firefox112', 'safari16.4', 'ios16.4'],
-  },
+    target: ['es2022', 'chrome112', 'edge112', 'firefox112', 'safari16.4', 'ios16.4']
+  }
 });
