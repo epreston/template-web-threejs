623b5b05f894e2f65fc79a7762f95e15220cca12
Ed Preston <epreston@prestonsoft.com>
2023-08-23 19:58:55 +1000

    Update vite.config.js
    compress only on production, which by default includes build

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,17 @@ import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // config options
-  assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.m4a'],
-  server: {
-    open: '/index.html'
-  },
-  plugins: [glsl({ compress: true })],
-  build: {
-    target: ['es2022', 'chrome112', 'edge112', 'firefox112', 'safari16.4', 'ios16.4']
-  }
+export default defineConfig(({ mode }) => {
+  /** @type {import('vite').UserConfig} */
+  return {
+    // config options
+    assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.m4a'],
+    server: {
+      open: '/index.html'
+    },
+    plugins: [glsl({ compress: mode === 'production' ? true : false })],
+    build: {
+      target: ['es2022', 'chrome112', 'edge112', 'firefox112', 'safari16.4', 'ios16.4']
+    }
+  };
 });
