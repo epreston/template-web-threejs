ce34249ed658e118451224f74e477723b45eda3a
Ed Preston <epreston@prestonsoft.com>
2023-11-06 03:18:04 +1100

    Update vite.config.js
    chunking and dependency size observability

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,26 @@ export default defineConfig(({ mode }) => {
   /** @type {import('vite').UserConfig} */
   return {
     // config options
-    assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.m4a'],
-    server: {
-      open: '/index.html'
-    },
+    assetsInclude: ['**/*.gltf', '**/*.glb'],
     plugins: [glsl({ compress: mode === 'production' ? true : false })],
     build: {
-      target: ['es2022', 'chrome112', 'edge112', 'firefox112', 'safari16.4', 'ios16.4']
+      target: ['es2022', 'chrome112', 'edge112', 'firefox112', 'safari16.4', 'ios16.4'],
+      chunkSizeWarningLimit: 600,
+      rollupOptions: {
+        output: {
+          // chunking and dependency size observability
+          manualChunks: (id) => {
+            // create chunk for three deps.
+            if (id.includes('node_modules/three') || id.includes('@three')) {
+              return 'three';
+            }
+            // create chunk for tweakpane deps.
+            if (id.includes('tweakpane') || id.includes('@tweakpane')) {
+              return 'tweakpane';
+            }
+          }
+        }
+      }
     }
   };
 });
